fc13413cfd8167b1491dbf03c95277fbff378c43
Vandana Bhat <vbhat2@apple.com>
2016-09-14 16:38:04 -0700

    index page re-design

--- index.js
+++ index.js
@@ -1,3 +1,86 @@
-/**
- * Created by vandana on 9/6/16.
- */
+$(document).ready(function () {
+
+    var top_nav_collapse = false;
+
+    $(window).scroll(function () {
+        var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        if ((scrollTop > 200 && top_nav_collapse) || (scrollTop < 200 && !top_nav_collapse)) {
+            return;
+        } else {
+            var nav_collpase_class = $('#nav-bar-top').hasClass("top-nav-collapse");
+            if (top_nav_collapse && nav_collpase_class) {
+                $('#nav-bar-top').removeClass('top-nav-collapse');
+                top_nav_collapse = false;
+            } else {
+                top_nav_collapse = true;
+                $('#nav-bar-top').addClass('top-nav-collapse');
+            }
+        }
+    });
+
+    function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+        var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+
+        return {
+            x: centerX + (radius * Math.cos(angleInRadians)),
+            y: centerY + (radius * Math.sin(angleInRadians))
+        };
+    }
+
+    function describeArc(x, y, radius, startAngle, endAngle) {
+
+        var start = polarToCartesian(x, y, radius, endAngle);
+        var end = polarToCartesian(x, y, radius, startAngle);
+
+        var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+
+        var d = [
+            "M", start.x, start.y,
+            "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+        ].join(" ");
+
+        return d;
+    }
+
+    document.getElementById("angularjs").setAttribute("d", describeArc(200, 200, 200, 0, 250));
+    document.getElementById("html").setAttribute("d", describeArc(200, 200, 175, 0, 310));
+    document.getElementById("js").setAttribute("d", describeArc(200, 200, 150, 0, 290));
+    document.getElementById("css").setAttribute("d", describeArc(200, 200, 125, 0, 270));
+    document.getElementById("jquery").setAttribute("d", describeArc(200, 200, 100, 0, 270));
+    document.getElementById("bootstrap").setAttribute("d", describeArc(200, 200, 75, 0, 330));
+
+    var changeText = function (e) {
+        var text = '';
+        switch (e.target.id){
+            case 'angularjs':
+                text = "AngularJS 70%";
+                break;
+            case 'css':
+                text = "CSS3 75%";
+                break;
+            case 'html':
+                text = "HTML5 85%";
+                break;
+            case 'js':
+                text = "Javascript 80%";
+                break;
+            case 'jquery':
+                text = "jQuery 75%";
+                break;
+            case 'bootstrap':
+                text = "Bootstrap 90%";
+                break;
+        }
+        $('#skillText').text(text);
+    };
+    var resetText = function () {
+        $('#skillText').text("Creativity 100%");
+    };
+
+
+    $(".skill-chart").on('mouseover','path',function(event) {
+        changeText(event);
+    }).on('mouseleave', 'path', function() {
+        resetText();
+    });
+})
--- waves/index.js
+++ waves/index.js
@@ -1,3 +1,90 @@
-/**
- * Created by vandana on 8/2/16.
- */
+// global vars
+var
+    // div = document.createElement('div'),
+    canvasContainerDiv = document.getElementById('canvasContainerDiv'),
+    canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d'),
+    w,
+    h;
+
+// events
+window.onresize = function(event) {
+    SetMargins();
+};
+
+// initialization
+function Init () {
+    // document.body.appendChild(div);
+    canvasContainerDiv.style.position = "fixed";
+    canvas.style.zIndex = "0";
+    canvas.style.position = "absolute";
+    canvasContainerDiv.appendChild(canvas);
+
+    SetMargins();
+    Update();
+}
+Init();
+
+
+// main loop
+function Update() {
+
+    ctx.fillStyle = '#00d4c8';
+    ctx.fillRect(0,0,w,h);
+
+    var timeCur = new Date().getTime();
+
+    var maxLayers = Math.floor(h / 150) + 1;
+    var waveLayer = -1;
+    var offset = 0;
+    var offsetInc = 30;
+
+    while(waveLayer < maxLayers){
+        var timeDivider = (8 - (5 * waveLayer / maxLayers));
+        var timeMod = timeCur / timeDivider;
+        var ampMod = 32 + 12 * waveLayer;
+        var ampMult = 8 + waveLayer * 4;
+
+        var grd = ctx.createLinearGradient(0,offset,0,offset + offsetInc * 2);
+        grd.addColorStop(0,'#999999');//'rgba(255,255,208,0.2)');
+        grd.addColorStop(0.5,'#666666');//'rgba(255,208,208,0)');
+        grd.addColorStop(1,'#333333');
+
+        ctx.beginPath();
+
+        for(var i = 0 ; i < w; i+= 10 ){
+            var timeUse = (timeMod + i) / ampMod;
+            var amp = ampMult * Math.sin(timeUse);
+            var height = 4 * Math.cos((timeMod) / 48);
+            var yPoint = amp - height + offset;
+            var xPoint = i;
+            ctx.lineTo(xPoint, yPoint);
+        }
+        ctx.lineTo(w,h+offset);
+        ctx.lineTo(0,h+offset);
+        ctx.lineTo(0,offset);
+
+        ctx.closePath();
+        ctx.fillStyle = grd;
+        ctx.fill();
+
+        waveLayer++;
+        offsetInc = 30 + 10 * Math.pow(waveLayer,2);
+        offset += offsetInc
+    }
+
+    requestAnimationFrame(Update);
+}
+
+
+// functions
+function SetMargins () {
+    var bodyW = document.documentElement.clientWidth,
+        bodyH = document.documentElement.clientHeight;
+
+    w = canvas.width = Math.max(600,bodyW);
+    h = canvas.height = bodyH/4;
+    canvas.style.bottom = 0;
+
+    canvasContainerDiv.style.left=canvasContainerDiv.style.right=canvasContainerDiv.style.top=canvasContainerDiv.style.bottom="0";
+}
