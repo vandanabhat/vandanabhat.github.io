d15acb5e93b57cca2de9e6e3633dc99f0a205cf2
Vandana Bhat <vbhat2@apple.com>
2016-09-22 11:37:13 -0700

    feat:About me page re-design

--- index.js
+++ index.js
@@ -42,13 +42,6 @@ $(document).ready(function () {
         return d;
     }
 
-    document.getElementById("angularjs").setAttribute("d", describeArc(200, 200, 200, 0, 250));
-    document.getElementById("html").setAttribute("d", describeArc(200, 200, 175, 0, 310));
-    document.getElementById("js").setAttribute("d", describeArc(200, 200, 150, 0, 290));
-    document.getElementById("css").setAttribute("d", describeArc(200, 200, 125, 0, 270));
-    document.getElementById("jquery").setAttribute("d", describeArc(200, 200, 100, 0, 270));
-    document.getElementById("bootstrap").setAttribute("d", describeArc(200, 200, 75, 0, 330));
-
     var changeText = function (e) {
         var text = '';
         switch (e.target.id){
@@ -76,6 +69,23 @@ $(document).ready(function () {
     var resetText = function () {
         $('#skillText').text("Creativity 100%");
     };
+    var findProjectElement = function (currentElm, nextElm, arrow) {
+        nextElm = nextElm.length === 0? (arrow === 'left'? $('.project-list li:last-child'): nextElm = $('.project-list li:first-child')):nextElm;
+        if( arrow == 'left'){
+            $(nextElm).addClass('fadeInRight');
+        }else {
+            $(nextElm).addClass('fadeInLeft')
+        }
+        $(currentElm).removeClass("show");
+        $(nextElm).addClass('show');
+    };
+
+    document.getElementById("angularjs").setAttribute("d", describeArc(200, 200, 170, 0, 250));
+    document.getElementById("html").setAttribute("d", describeArc(200, 200, 150, 0, 310));
+    document.getElementById("js").setAttribute("d", describeArc(200, 200, 130, 0, 290));
+    document.getElementById("css").setAttribute("d", describeArc(200, 200, 110, 0, 270));
+    document.getElementById("jquery").setAttribute("d", describeArc(200, 200, 90, 0, 270));
+    document.getElementById("bootstrap").setAttribute("d", describeArc(200, 200, 70, 0, 330));
 
 
     $(".skill-chart").on('mouseover','path',function(event) {
@@ -83,4 +93,63 @@ $(document).ready(function () {
     }).on('mouseleave', 'path', function() {
         resetText();
     });
+
+    $("#arrow-left-id").on('click', function () {
+        var currentElm = $('.project-list').find(".show");
+        var  prevElm = currentElm.prev();
+        prevElm = prevElm.length === 0? $('.project-list li:last-child') : prevElm;
+        $(prevElm).addClass('fadeInRight').addClass('show');
+        $(currentElm).removeClass("show");
+    });
+    $("#arrow-right-id").on('click', function () {
+        var currentElm = $('.project-list').find(".show");
+        var  nextElm = currentElm.next();
+        nextElm = nextElm.length === 0? $('.project-list li:first-child') : nextElm;
+        $(nextElm).addClass('fadeInLeft').addClass('show');
+        $(currentElm).removeClass("show");
+    });
+
+    $('.contactForm').on("keyup",'input, textarea', function () {
+        if($(this).val().length > 0){
+            $(this).addClass("has-value");
+        }
+    });
+
+    var greetArr = ['',"Hello","नमस्ते",'ನಮಸ್ಕಾರ'],
+        cursor = $('#cursor'),
+        greeting = $('#greeting'),
+        counter = 500,
+        len = greetArr.length,
+        i=0,
+        intervalId;
+
+    var changeGreetingText = function(text,j){
+        var text = text;
+        var index= j;
+        setTimeout( function () {
+            $(greeting).text($(greeting).text() + text);
+        },1000 + index * 100);
+
+    };
+    setInterval(function () {
+        i = i >= len-1 ? 0 : i+1;
+        $(greeting).text('');
+        var subArr = greetArr[i].split('');
+            for (var j=0;j<subArr.length;j++){
+                changeGreetingText(subArr[j],j);
+            }
+    }, (greetArr[i].length+1)*1000+2000);
+
+    setInterval(function () {
+        $(cursor).text( $(cursor).text() == '' ? "|":'' );
+    },1000);
+
 })
+function sendEmail() {
+    $.ajax({
+        url: "https://formspree.io/vandanak.bhat@gmail.com",
+        method: "POST",
+        data: {message: "hello!",email:"test@gmail.com"},
+        dataType: "json"
+    });
+}
